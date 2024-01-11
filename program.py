65446c664142ba131016465beb22927d2d514025
megahomyak <g.megahomyak@gmail.com>
2024-01-12 02:43:11 +0500

    Bug fixes

--- program.py
+++ program.py
@@ -1,27 +1,20 @@
 from vkbottle import User
-import asyncio
 from random import randint
+from hampst import *
 
-ids = [int(id_) for id_ in open("profile_ids.txt").read().strip().splitlines()]
+ids = ints(read("profile_ids.txt").splitlines())
 
-PEER_IDS_LIMIT = 100
-
-user_batches = [
-    ids[i:i + PEER_IDS_LIMIT]
-    for i in range(0, len(ids), PEER_IDS_LIMIT)
-]
-
-message = open("message.txt").read().strip()
-user_token = open("user_token.txt").read().strip()
+message = read("message.txt")
+user_token = read("user_token.txt")
 
 async def main():
     client = User(token=user_token)
-    for user_batch in user_batches:
+    for peer_id in ids:
         await client.api.messages.send(
             message=message,
-            peer_ids=user_batch,
+            peer_id=peer_id,
             random_id=randint(-1_000_000, 1_000_000)
         )
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.25)
 
 asyncio.run(main())
